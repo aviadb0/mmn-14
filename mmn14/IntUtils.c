bf109d572a2883a3be0ffb58b3d6d6430390ca21
MotiDavid100 <41124651+MotiDavid100@users.noreply.github.com>
2018-07-15 18:04:43 +0300

    Update IntUtils.c

--- mmn14/IntUtils.c
+++ mmn14/IntUtils.c
@@ -1,25 +1,49 @@
 #include "IntUtils.h"
+
 #include "GeneralUtils.h"
 
+
+
 /*
-	Util to convert an int with padded zeros to buffer in Base64
+
+	Util to convert an int with padded zeros to buffer in Base 2
+
 */
-char* convertIntToBase64(int value, char buf[MAX_BASE4_INT_LEN + 1], int padZeros)
+
+char* convertIntToBase2(int value, char buf[MAX_BASE2_INT_LEN + 1], int padZeros)
+
 {
+
 	int base = OUTPUT_BASE,i = 0;
-	value = value & MAX_BASE4_VALUE;
+
+	value = value & MAX_BASE2_VALUE;
+
+
 
 	while (value) {
-		buf[i++] = (value % base) + BASE4_ZERO_VALUE;
+
+		buf[i++] = (value % base) + BASE2_ZERO_VALUE;
+
 		value = value / base;
+
 	}
+
 	if (padZeros) {
-		for (; i < MAX_BASE4_INT_LEN; )
+
+		for (; i < MAX_BASE2_INT_LEN; )
+
 		{
-			buf[i++] = BASE4_ZERO_VALUE;
+
+			buf[i++] = BASE2_ZERO_VALUE;
+
 		}
+
 	}
+
 	buf[i] = BACKSLACH_ZERO;
+
 	reverseString(buf);
+
 	return buf;
+
 }
